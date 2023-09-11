18fb890c37be776f79befb6fff330a09d4005492
Krishna Chandra Maurya <91846306+imkrishna-23@users.noreply.github.com>
2023-09-11 23:24:23 +0530

    Add files via upload

---
+++ TicTacToeGUI.java
@@ -0,0 +1,110 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToeGUI extends JFrame implements ActionListener {
+    private static final int SIZE = 3;
+    private JButton[][] buttons;
+    private char currentPlayer;
+
+    public TicTacToeGUI() {
+        setTitle("Tic-Tac-Toe");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setLocationRelativeTo(null);
+
+        buttons = new JButton[SIZE][SIZE];
+        currentPlayer = 'X';
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(SIZE, SIZE));
+
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                buttons[row][col] = new JButton("");
+                buttons[row][col].setFont(new Font("Arial", Font.PLAIN, 48));
+                buttons[row][col].setFocusPainted(false);
+                buttons[row][col].addActionListener(this);
+                panel.add(buttons[row][col]);
+            }
+        }
+
+        add(panel);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton buttonClicked = (JButton) e.getSource();
+
+        if (buttonClicked.getText().equals("")) {
+            buttonClicked.setText(String.valueOf(currentPlayer));
+            buttonClicked.setEnabled(false);
+            if (checkWin()) {
+                JOptionPane.showMessageDialog(this, "Player " + currentPlayer + " wins!");
+                resetBoard();
+            } else if (isBoardFull()) {
+                JOptionPane.showMessageDialog(this, "It's a draw!");
+                resetBoard();
+            } else {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        }
+    }
+
+    private boolean checkWin() {
+        // Check rows, columns, and diagonals for a win
+        for (int i = 0; i < SIZE; i++) {
+            if (buttons[i][0].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[i][1].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[i][2].getText().equals(String.valueOf(currentPlayer))) {
+                return true;
+            }
+            if (buttons[0][i].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[1][i].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[2][i].getText().equals(String.valueOf(currentPlayer))) {
+                return true;
+            }
+        }
+
+        if (buttons[0][0].getText().equals(String.valueOf(currentPlayer)) &&
+            buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
+            buttons[2][2].getText().equals(String.valueOf(currentPlayer))) {
+            return true;
+        }
+
+        if (buttons[0][2].getText().equals(String.valueOf(currentPlayer)) &&
+            buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
+            buttons[2][0].getText().equals(String.valueOf(currentPlayer))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (buttons[row][col].getText().equals("")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void resetBoard() {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                buttons[row][col].setText("");
+                buttons[row][col].setEnabled(true);
+            }
+        }
+        currentPlayer = 'X';
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new TicTacToeGUI());
+    }
+}
